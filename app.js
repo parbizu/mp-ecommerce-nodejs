8f4944c6ae9e76c90ac77da99f4264eab631cb72
parbizuc <parbizu@spsolutions.com.mx>
2020-10-04 17:44:27 -0500

    express

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+
+
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
@@ -18,13 +31,4 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
-//app.listen(3000);
-const server = require('http').createServer();
-//const port = process.env.PORT || 3000;
-//server.listen(port, () => console.log(`Listening on ${port}`));
-
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+app.listen(3000);
