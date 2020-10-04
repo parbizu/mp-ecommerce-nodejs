0001d2310f7d3fd2e0c83820eeb9457b088ec882
parbizuc <parbizu@spsolutions.com.mx>
2020-10-04 17:35:13 -0500

    express

--- app.js
+++ app.js
@@ -21,5 +21,10 @@ app.use('/assets', express.static(__dirname + '/assets'));
 //app.listen(3000);
 const server = require('http').createServer();
 //const port = process.env.PORT || 3000;
-const port = 3000;
-server.listen(port, () => console.log(`Listening on ${port}`));
+//server.listen(port, () => console.log(`Listening on ${port}`));
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
