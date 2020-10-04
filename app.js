8d21da5808770bbd02738ce7e0569a2f387e2b63
parbizuc <parbizu@spsolutions.com.mx>
2020-10-04 18:29:54 -0500

    express

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var exphbs  = require('express-handlebars');
 const mercadopago = require ('mercadopago');
 
 mercadopago.configure({
-    access_token: 'APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181'
+    access_token: 'APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181',
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
   });
  
+
+
  
 var app = express();
  
