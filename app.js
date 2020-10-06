1c598a268885d7a8633404cc0e82361d208e1abb
parbizuc <parbizu@spsolutions.com.mx>
2020-10-05 20:58:18 -0500

    express

--- app.js
+++ app.js
@@ -58,8 +58,67 @@ app.post('/order', function (req, res) {
         }
     }
 
- 
     
+    let preference = {
+        "items": [
+            {
+                "id": "item-ID-1234",
+                "title": "Mi producto",
+                "currency_id": "MXN",
+                "picture_url": "https://www.mercadopago.com/org-img/MP3/home/logomp3.gif",
+                "description": "Descripción del Item",
+                "category_id": "art",
+                "quantity": 1,
+                "unit_price": 75.76
+            }
+        ],
+        "payer": {
+            "name": "Juan",
+            "surname": "Lopez",
+            "email": "user@email.com",
+            "phone": {
+                "area_code": "11",
+                "number": "4444-4444"
+            },
+            "identification": {
+                "type": "DNI",
+                "number": "12345678"
+            },
+            "address": {
+                "street_name": "Street",
+                "street_number": 123,
+                "zip_code": "5700"
+            }
+        },
+        "back_urls": {
+            "success": "https://www.success.com",
+            "failure": "http://www.failure.com",
+            "pending": "http://www.pending.com"
+        },
+        "auto_return": "approved",
+        "payment_methods": {
+            "excluded_payment_methods": [
+                {
+                    "id": "master"
+                }
+            ],
+            "excluded_payment_types": [
+                {
+                    "id": "ticket"
+                }
+            ],
+            "installments": 12
+        },
+        "notification_url": "https://www.your-site.com/ipn",
+        "statement_descriptor": "MINEGOCIO",
+        "external_reference": "Reference_1234",
+        "expires": true,
+        "expiration_date_from": "2016-02-01T12:00:00.000-04:00",
+        "expiration_date_to": "2016-02-28T12:00:00.000-04:00"
+    }
+
+
+    /*
      let preference = {
      items: [
        {
@@ -71,18 +130,22 @@ app.post('/order', function (req, res) {
          external_reference : 'plinio.arbizu@gmail.com'
        }
      ],
-     payer : payer
+     payer : payer,
+     notification_url : 'http://urlmarketplace.com/notification_ipn'
      };
-   
+     */
  
      //d) URL Imagen: Foto del producto seleccionado. (url válida)
+     
      
    
      mercadopago.preferences.create(preference)
      .then(function(response){
-     // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+        console.log('aca d');
+        // Este valor reemplazará el string "<%= global.id %>" en tu HTML
      global.id = response.body.id;
      }).catch(function(error){
+         console.log('aca');
      console.log(error);
      });
    
